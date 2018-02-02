3a01228b1447496595761656bf5c83a828c04cc9
Javi Manzano <javi.manzano.oller@gmail.com>
2018-02-02 09:37:52 +0000

    Add util function, fetchComicDetails, for getting the number of pages of a comic #5

--- app/src/main/java/com/jgasteiz/readcomicsandroid/helpers/Utils.kt
+++ app/src/main/java/com/jgasteiz/readcomicsandroid/helpers/Utils.kt
@@ -3,6 +3,7 @@ package com.jgasteiz.readcomicsandroid.helpers
 import android.content.Context
 import android.net.ConnectivityManager
 import android.util.Log
+import com.jgasteiz.readcomicsandroid.interfaces.OnComicDetailsFetched
 import com.jgasteiz.readcomicsandroid.interfaces.OnDirectoryContentFetched
 import com.jgasteiz.readcomicsandroid.interfaces.OnResponseFetched
 import com.jgasteiz.readcomicsandroid.models.Item
@@ -17,9 +18,14 @@ object Utils {
 
     private val LOG_TAG = Utils::class.java.simpleName
 
+    // TODO: move this to some settings.
     private val DIRECTORY_API_URL = "http://192.168.0.28/api/directory/"
+    private val COMIC_DETAIL_API_URL = "http://192.168.0.28/api/comic/"
     private val PAGE_API_URL = "http://192.168.0.28/api/page/"
 
+    /**
+     * Returns whether there's a network available.
+     */
     fun isNetworkAvailable(context: Context): Boolean {
         val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
         val activeNetworkInfo = connectivityManager.activeNetworkInfo
@@ -66,9 +72,35 @@ object Utils {
         else {
             task.execute(DIRECTORY_API_URL)
         }
-
     }
 
+    /**
+     * Fetch all the directories and comics inside a directory.
+     * @param onComicDetailsFetched callback
+     */
+    fun fetchComicDetails(context: Context, comicPath: String, onComicDetailsFetched: OnComicDetailsFetched) {
+        val task = GetStringResponseAsyncTask(object : OnResponseFetched {
+            override fun callback(response: String) {
+                var numPages = -1
+
+                Log.d(LOG_TAG, response)
+                try {
+                    val responseJson = JSONObject(response)
+                    numPages = responseJson.get("num_pages") as Int
+                } catch (e: JSONException) {
+                    Log.e(LOG_TAG, e.message)
+                }
+
+                onComicDetailsFetched.callback(numPages)
+            }
+        })
+
+        task.execute(String.format("%s%s/", COMIC_DETAIL_API_URL, comicPath))
+    }
+
+    /**
+     * Return the url for downloading the given comic on the given page number.
+     */
     fun getPageUrl(comic: Item, pageNumber: Int): String {
         return String.format("%s%s/%s", PAGE_API_URL, comic.path, pageNumber)
     }
---
+++ app/src/main/java/com/jgasteiz/readcomicsandroid/interfaces/OnComicDetailsFetched.kt
@@ -0,0 +1,5 @@
+package com.jgasteiz.readcomicsandroid.interfaces
+
+interface OnComicDetailsFetched {
+    fun callback(numPages: Int)
+}
